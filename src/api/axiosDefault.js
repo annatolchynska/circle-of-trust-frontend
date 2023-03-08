bb241f73466997dfa62f78bae9b3af2e50f5ebec
annatolchynska <anna.t1417@gmail.com>
2023-03-08 18:16:36 +0000

    Add axios interceptors to refresh tokens.

---
+++ src/api/axiosDefault.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// Configuration for axios
+axios.defaults.baseURL = 'https://circle-of-trust.herokuapp.com/';
+// axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+// Axios instances to be used as interceptors
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
