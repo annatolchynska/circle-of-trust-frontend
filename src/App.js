cf27eafe983958f3ed7141532023660ac233efa1
annatolchynska <anna.t1417@gmail.com>
2023-03-08 16:39:41 +0000

    Implement small Navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,96 @@
-import './App.css';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import NavBarSmall from './components/NavBarSmall';
+import { NavLink, Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
+import MemoCreateForm from './pages/memo/MemoCreateForm';
+import ProfilePage from './pages/profiles/ProfilePage';
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import MemoPostPage from './pages/memo/MemoPostPage';
+import MemoPostsPage from './pages/memo/MemoPostsPage';
+import MemoEditForm from './pages/memo/MemoEditForm';
+import TodoCreateForm from './pages/to_do/TodoCreateForm';
+import TodoPostPage from './pages/to_do/TodoPostPage';
+import TodoPostsPage from './pages/to_do/TodoPostsPage';
+import TodoEditForm from './pages/to_do/TodoEditForm';
+import About from './pages/About';
+import { Col, Row } from 'react-bootstrap';
+import FamilyProfiles from './pages/profiles/FamilyProfiles';
+import NotFound from './components/NotFound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
-    </div>
+    div className = { styles.App } >
+      {/* heading */ }
+      < Row >
+      {/*large screens*/ }
+      < Col className = {`${styles.HeadingLgScreen} ${styles.Heading}`
+}>
+  <NavLink to="/" className={styles.HeadingLink}>
+    <h1>Appy Families</h1>
+  </NavLink>
+        </Col >
+
+  {/* small screens */ }
+  < Col sm = { 12} className = {`${styles.SmHeading} ${styles.NavSmScreen}`}>
+    <NavBarSmall />
+        </Col >
+      </Row >
+      <br />
+
+      <Row className={styles.MainContent}>
+        {/*Navbar */}
+        <Col lg={3} className={`${styles.LeftSide} ${styles.NavLgScreen}`}>
+          < NavBar />
+        </Col>
+
+        {/*main content*/}
+        <Col sm={12} md={8} lg={6} className={styles.Middle}>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <MemoPostsPage message="No results found. Adjust the search keyword." />
+              )}
+            />
+            <Route
+              exact
+              path="/to_do"
+              render={() => (
+                <TodoPostsPage message="No results found. Adjust the search keyword" />
+              )}
+            />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
+            <Route exact path="/about" render={() => <About />} />
+            <Route exact path="/login" render={() => <SignInForm />} />
+            <Route exact path="/memo/create" render={() => <MemoCreateForm />} />
+            <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+            <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+            <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
+            <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
+            <Route exact path="/memo_posts/:id" render={() => <MemoPostPage />} />
+            <Route exact path="/memo_posts/:id/edit" render={() => <MemoEditForm />} />
+            <Route exact path="/to_do/create" render={() => <TodoCreateForm />} />
+            <Route exact path="/to_do/:id" render={() => <TodoPostPage />} />
+            <Route exact path="/to_do/:id/edit" render={() => <TodoEditForm />} />
+            <Route render={() => <NotFound />} />
+          </Switch>
+        </Col>
+
+        {/* Family profiles*/}
+        <Col md={4} lg={3} className={styles.RightSide}>
+          <FamilyProfiles />
+        </Col>
+        </Row>
+    </div >
+
   );
 }
 
---
+++ src/components/NavBarSmall.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import navStyles from '../styles/NavBar.module.css';
+import styles from '../styles/NavBarSmall.module.css';
+import { Link, NavLink } from 'react-router-dom';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import axios from 'axios';
+import Avatar from './Avatar';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
+import { removeTokenTimestamp } from '../utils/utils';
+
+const NavBar = () => {
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+            removeTokenTimestamp();
+        } catch (err) {
+            //console.log(err);
+        }
+    };
+
+    const loggedInIcons = (
+        <>
+            <NavLink
+                to='/memo/create'
+                className={navStyles.NavLink}
+                activeClassName={navStyles.Active}
+                aria-label='create a memo'
+                rel='noreferrer'
+            >
+                <i className="fas fa-plus-circle"></i>
+                {' '}
+                Create Memo
+            </NavLink>
+            <NavLink
+                to='/to_do/create'
+                className={navStyles.NavLink}
+                activeClassName={navStyles.Active}
+                aria-label='create a todo item'
+                rel='noreferrer'
+            >
+                <i className="fas fa-plus-circle"></i>
+                {' '}
+                Create a Task
+            </NavLink>
+            <NavLink
+                to="/"
+                className={navStyles.NavLink}
+                onClick={handleSignOut}
+                aria-label='sign out'
+                rel='noreferrer'
+            >
+                <i className="fas fa-sign-out-alt"></i>
+                {' '}
+                Sign out
+            </NavLink>
+            <NavLink
+                to={`/profiles/${currentUser?.profile_id}`}
+                className={navStyles.NavLink}
+                aria-label='user profile'
+                rel='noreferrer'
+            >
+                <Avatar src={currentUser?.profile_image} text="Profile" height={35} />
+            </NavLink>
+        </>
+    )
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                to="/login"
+                className={navStyles.NavLink}
+                activeClassName={navStyles.Active}
+                aria-label="login"
+                rel="noreferrer"
+            >
+                <i className="fas fa-sign-in-alt"></i>
+                {''}
+                Login
+            </NavLink>
+            <NavLink
+                to="/signup"
+                className={navStyles.NavLink}
+                activeClassName={navStyles.Active}
+                aria-label="sign up"
+                rel="noreferrer"
+            >
+                <i className="fas fa-user-plus"></i>
+                {' '}
+                Signup
+            </NavLink>
+            <NavLink
+                to="/about"
+                className={navStyles.NavLink}
+                activeClassName={navStyles.Active}
+                aria-label="about page"
+                rel="noreferrer"
+            >
+                <i className='fas fa-info-circle'></i>
+                {' '}
+                About
+            </NavLink>
+        </>
+    );
+    return (
+        <div className={styles.Container}>
+            <Navbar className={styles.Background} expand="lg" expanded={expanded}>
+                <Container className={styles.NavBar}>
+                    <Link to="/" className={styles.Heading}>
+                        <Navbar.Brand className={styles.Move}>
+                            <h1>Circle of trust</h1>
+                        </Navbar.Brand>
+                    </Link>
+                    <Navbar.Toggle
+                        ref={ref}
+                        onClick={() => setExpanded(!expanded)}
+                        aria-controls="basic-navbar-nav"
+                    />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto flex-column text-left">
+                            <NavLink
+                                exact
+                                to="/"
+                                className={styles.NavLink}
+                                activeClassName={styles.Active}
+                                aria-label='home page memos'
+                                rel='noreferrer'
+                            >
+                                <i className="far fa-sticky-note"></i>
+                                {' '}
+                                Memo
+                            </NavLink>
+                            <NavLink
+                                to="/to_do"
+                                className={styles.NavLink}
+                                activeClassName={styles.Active}
+                                aria-label='todo page'
+                                rel='noreferrer'
+                            >
+                                <i className="fas fa-list"></i>
+                                {' '}
+                                To Do List
+                            </NavLink>
+
+                            {currentUser ? loggedInIcons : loggedOutIcons}
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NavBar;
