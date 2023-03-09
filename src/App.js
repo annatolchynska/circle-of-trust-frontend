e3a31aef70f4fb5fb25bbbce75baca53ee1f2bec
annatolchynska <anna.t1417@gmail.com>
2023-03-09 20:01:25 +0000

    Add more styling, create profiles.

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         </Col>
 
         {/*main content*/}
-        <Col sm={12} md={8} lg={6} className={styles.Middle}>
+        <Col sm={12} md={4} lg={3} className={styles.Middle}>
           <Switch>
             <Route
               exact
